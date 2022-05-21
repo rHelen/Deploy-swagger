b957432934947595d8a358a806698e46ac7e6817
Helen <rutehelenrh@gmail.com>
2022-05-21 10:59:18 -0300

    Ajustes

---
+++ src/main/java/org/generation/blogPessoal/controller/TemaController.java
@@ -0,0 +1,58 @@
+package org.generation.blogPessoal.controller;
+
+import java.util.List;
+
+import org.generation.blogPessoal.model.Postagem;
+import org.generation.blogPessoal.model.Tema;
+import org.generation.blogPessoal.repository.TemaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tema")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class TemaController {
+	
+	@Autowired
+	private TemaRepository repositoty;
+	
+	@GetMapping
+	public ResponseEntity<List<Tema>> GetAll(){
+		return ResponseEntity.ok(repositoty.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Tema> GetById (@PathVariable Long id){
+		return repositoty.findById(id)
+				.map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
+	@GetMapping("/descricao/{descricao}")
+	public ResponseEntity<List<Tema>> GetByDescricao(@PathVariable String descricao){
+		return ResponseEntity.ok(repositoty.findAllByDescricaoContainingIgnoreCase(descricao));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Tema> post (@RequestBody Tema tema){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repositoty.save(tema));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Tema> put (@RequestBody Tema tema){
+		return ResponseEntity.status(HttpStatus.OK).body(repositoty.save(tema));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete (@PathVariable Long id) {
+		repositoty.deleteById(id);
+	}
+}
---
+++ src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -0,0 +1,92 @@
+package org.generation.blogPessoal.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+@Entity
+@Table(name = "postagem")
+public class Postagem {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message="O Título é obrigatório")
+	@Size(min = 5, max = 1000)
+	private String titulo;
+	
+	@NotBlank(message="O Texto é obrigatório")
+	@Size(min = 5, max = 1500)
+	private String texto;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date data = new java.sql.Date(System.currentTimeMillis());
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+}
