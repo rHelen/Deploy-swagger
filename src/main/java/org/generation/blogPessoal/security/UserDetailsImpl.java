7aa94d9cda0675fc96624caaa88e56ba7199f67d
Helen <rutehelenrh@gmail.com>
2022-05-21 11:08:17 -0300

    Ajustes

--- src/main/java/org/generation/blogPessoal/security/UserDetailsImpl.java
+++ src/main/java/org/generation/blogPessoal/security/UserDetailsImpl.java
@@ -9,7 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.generation.blogPessoal.model.Usuario;
 public class UserDetailsImpl implements UserDetails{
 
-	private static final long serialVersionUID =1L;
+	private static final long serialVersionUID = 1L;
 		
 		private String userName;
 		private String password;
